eee9adac0faf4d6f19ce2dab5f33674ebfd3518d
Monit Jangir <monitjangir@Monits-MacBook.local>
2025-08-22 23:19:15 +0530

    fix: Update framer-motion variants for TypeScript compatibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { type: 'ease', duration: 0.8 },
+      transition: { type: 'tween', ease: 'easeOut' duration: 0.8 },
     },
   };
   const bottomElementVariants = {
@@ -58,7 +58,7 @@ export default function Home() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { type: 'ease', duration: 0.8, delay: 0.2 },
+      ttransition: { type: 'tween', ease: 'easeOut', duration: 0.8, delay: 0.2 },
     },
   };
 
